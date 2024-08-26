1d8189db8562f997655053986c8c10be27016183
Your Name <you@example.com>
2024-08-27 02:09:45 +0300

    back-end

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,17 @@
 from rest_framework import viewsets
 from .models import Project
 from .serializers import ProjectSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter projects by the custom user who created them
+        return Project.objects.filter(created_by=self.request.user)
 
     def perform_create(self, serializer):
-        # Set the 'created_by' field to the currently authenticated user
+        # Automatically set the 'created_by' field to the currently authenticated custom user
         serializer.save(created_by=self.request.user)
